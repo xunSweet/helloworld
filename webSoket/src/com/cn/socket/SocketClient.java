6cfdef3bbb4c063efc78310196ac6e07e8b1b526
zhangshaojie <1172618241@qq.com>
2019-06-05 10:29:12 +0800

    第一次提交代码

---
+++ webSoket/src/com/cn/socket/SocketClient.java
@@ -0,0 +1,27 @@
+package com.cn.socket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SocketClient {
+	
+    public static void main(String[] args) {
+    	 // 要连接的服务端IP地址和端口
+    	String host = "127.0.0.1";
+    	int port = 55533;
+    	// 与服务端建立连接
+    	try {
+			Socket socket = new Socket(host, port);
+			// 建立连接后获得输出流
+			OutputStream outputStream  = socket.getOutputStream();
+		    String message="你好  yiwangzhibujian,张少杰，你是最帅的";
+			socket.getOutputStream().write(message.getBytes("UTF-8"));
+			outputStream.close();
+		    socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
